fece0d9d170f561e94ba31b5063efe8211ab6986
jhoule42 <jphoule29@hotmail.com>
2020-05-18 12:03:39 -0500

    Update

--- plot_all-sky.py
+++ plot_all-sky.py
@@ -10,7 +10,7 @@ import hdftools as hdf
 
 # Elements de la Matrice
 m = np.zeros((7, 12, 10))  # Matrice de (el, az, wl)
-elevation = [5, 15, 30, 45, 60, 75, 90]
+elevation = [1, 15, 30, 45, 60, 75, 90]
 azimuth = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330]
 wavelength = [397.0, 432.0, 467.0, 502.0, 537.0, 572.0, 607.0, 642.0, 677.0, 712.0]
 
@@ -19,32 +19,31 @@ listes = [elevation, azimuth, wavelength]
 
 # SCÉNARIOS de conversions
 path = "git/Illumina-scripts/Results-Sherbrooke"
-S_Actu_Ete = path + "/ete/data.txt"
-S_Actu_Hiver = path + "/hiver/data.txt"
-
-# S_2200k_Ete = "Documents/Results_alt_scen/Results/2200k_ete/data.txt"
-# S_2200k_Hiver = "Documents/Results_alt_scen/Results/2200k_hiver/data.txt"
-# S_2700k_Ete = "Documents/Results_alt_scen/Results/2700k_ete/data.txt"
-# S_2700k_Hiver =  "Documents/Results_alt_scen/Results/2700k_hiver/data.txt"
-# S_3000k_Ete = "Documents/Results_alt_scen/Results/3000k_ete/data.txt"
-# S_3000k_Hiver = "Documents/Results_alt_scen/Results/3000k_hiver/data.txt"
-# S_Ambree_Ete = "Documents/Results_alt_scen/Results/Ambree_ete/data.txt"
-# S_Ambree_Hiver = "Documents/Results_alt_scen/Results/Ambree_hiver/data.txt"
+S_Actu_Ete =  path + "/Actu_ete/data.txt"
+# S_Actu_Hiver = path + "/Actu_hiver/data.txt"
+# S_2200k_Ete = path + "/2200k_ete/data.txt"
+# S_2200k_Hiver = path + "/2200k_hiver/data.txt"
+# S_2700k_Ete = path + "/2700k_ete/data.txt"
+# S_2700k_Hiver = path + "/2700k_hiver/data.txt"
+# S_3000k_Ete = path + "/3000k_ete/data.txt"
+# S_3000k_Hiver = path + "/3000k_hiver/data.txt"
+# S_Ambree_Ete = path + "/Ambree_ete/data.txt"
+# S_Ambree_Hiver = path + "/Ambree_hiver/data.txt"
 
 liste_scenarios = [S_Actu_Ete, S_Actu_Hiver]
 nom_scenarios = ["Actu_Ete", "Actu_Hiver"]
 
-
 # liste_scenarios = [S_Actu_Ete, S_Actu_Hiver, S_2200k_Ete, S_2200k_Hiver, S_2700k_Ete, S_2700k_Hiver,
-# #                        S_3000k_Ete, S_3000k_Hiver, S_Ambree_Ete, S_Ambree_Hiver]
+#                         S_3000k_Ete, S_3000k_Hiver, S_Ambree_Ete, S_Ambree_Hiver]
 # nom_scenarios  = ["Actu_Ete", "Actu_Hiver", "2200k_Ete", "2200k_Hiver", "2700k_Ete", "2700k_Hiver",
 #                       "3000k_Ete", "3000k_Hiver", "Ambree_Ete", "Ambree_Hiver"]
 
-#a = MSD.Open("Documents/Results_Sherbrooke/Results/ete/elevation_angle_90-azimuth_angle_0-wavelength_397.0.hdf5")
+
+b = MSD.Open(path + "/Actu_ete/elevation_angle_90-azimuth_angle_0-wavelength_397.0.hdf5")
 
 # SPECTRES d'Integration
-scoto = "scotopic.dat"
-#photo = "Documents/Spectres/photopic.dat"
+path_spct = "git/Illumina-scripts/Spectres"
+scoto = path_spct + "/scotopic.dat"
 
 liste_spectres = [scoto]
 nom_spectres = ['scotopic']
@@ -62,8 +61,10 @@ for index1, scenario in enumerate(liste_scenarios):    # Itérations a travers s
             m[listes[0].index(elevation), listes[1].index(azimuth), listes[2].index(wavelength)] = valeur
             m[-1, :, :] = m[-1, 0, :]  # Lorsqu'on est au zénith
 
+            #print("{}".format(line))
 
-elevation = [5, 15, 30, 45, 60, 75, 90]  # Ouin je triche un peu my bad
+
+elevation = [1, 15, 30, 45, 60, 75, 90]  # Ouin je triche un peu my bad
 azimuth = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330]
 
 # Intégration sur les spectres
@@ -82,7 +83,7 @@ for index2, spct in enumerate(liste_spectres):
 
     # Intégration sur les longueurs d'ondes
     sky = (moy_wl[1]-moy_wl[0]) * np.dot(m, moy_spectre)
-    carte = (moy_wl[1]-moy_wl[0]) * np.dot(a, moy_spectre)
+#    carte = (moy_wl[1]-moy_wl[0]) * np.dot(b, moy_spectre)
 
 
     # Plot dans la fonction pour la carte du ciel
